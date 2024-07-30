7e179ac3e2d3e199e64b65ebc51204b7b52b7be7
Rohit <rohitjadhav.604.2@gmail.com>
2024-07-31 01:56:21 +0530

    solve some normal error

--- Eva/settings.py
+++ Eva/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'Eva.wsgi.application'
 
 if not DEBUG:
     DATABASES = {
-        'default': { dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+        'default':  dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 else:
